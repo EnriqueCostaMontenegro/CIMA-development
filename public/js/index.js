d44d2e6c91927426f2c70418d05e918866420390
kike <kike@gti.uvigo.es>
2023-01-18 10:48:55 +0100

    Updated version and readme
    18-01-2023

--- public/js/index.js
+++ public/js/index.js
@@ -20,7 +20,7 @@ var pdf = {
 var isFirstLoad = true;
 var PDFJS = null;
 var pdfjsWorker = null;
-var version = "1.2.08dev";
+var version = "1.2.08";
 const dictionary = getDictionary();
 
 var formFieldsValue= [ "validationTipoActividadCultural", "validationActivityName", "validationShortDescription", "validationFechaInicioDatePicker", 
